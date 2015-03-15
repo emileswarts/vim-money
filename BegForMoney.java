8bdbc5c9def85888e5e7f1010ddf79fdd737e927
Emile <emile.swarts123@gmail.com>
2015-03-15 21:10:27 +0000

    Rename classes

--- BegForMoney.java
+++ BegForMoney.java
@@ -1,9 +1,9 @@
 import javax.swing.*;
 
-public class Main {
+public class BegForMoney {
     public static void main(String[] args) {
 
-      final ImageIcon icon = new ImageIcon("vim-logo-en.png");
+      final ImageIcon icon = new ImageIcon("/Users/korpz/.vim/bundle/vim-money/vim-logo-en.png");
 
       String msg = "<html><b>Hello! Thanks for trying out VIM.</b><br /><br />This is an unregistered evaluation version, <br /><br />and although the trial is untimed, a license <br /><br />must be purchased for continued use.<br /><br />Would you like to purchase a license now?</b></html>";
 
