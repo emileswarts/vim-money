ab19ac25847d465de44a149530d75ad0d11009ad
Emile <emile.swarts123@gmail.com>
2015-03-15 20:24:07 +0000

    Create payment dialog

---
+++ Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+      final ImageIcon icon = new ImageIcon("vim-logo-en.png");
+
+      String msg = "<html><b>Hello! Thanks for trying out VIM.</b><br /><br />This is an unregistered evaluation version, <br /><br />and although the trial is untimed, a license <br /><br />must be purchased for continued use.<br /><br />Would you like to purchase a license now?</b></html>";
+
+      JLabel label = new JLabel(msg);
+      JOptionPane.showMessageDialog(null, msg, "Pay", JOptionPane.INFORMATION_MESSAGE, icon);
+  }
+}
